3ce9b9b62a2b3b41dbd9d1b016543fca3453f9d6
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-30 13:45:00 +0200

    refactor: extract constants

--- 01.codaBuzz/src/codaBuzz.c
+++ 01.codaBuzz/src/codaBuzz.c
@@ -3,14 +3,21 @@
 #include <stddef.h>
 #include <stdio.h>
 
+const int lowerBound = 1;
+const int greaterBound = 100;
+
 const char *number_to_string(const int n) {
     static char buffer[12];
     snprintf(buffer, sizeof(buffer), "%d", n);
     return buffer;
 }
 
+int is_out_of_range(const int n) {
+    return n < lowerBound || n > greaterBound;
+}
+
 const char *convert(const int n) {
-    if (n < 1 || n > 100) {
+    if (is_out_of_range(n)) {
         return NULL;
     }
     if (n % 15 == 0) return "CodaBuzz";
