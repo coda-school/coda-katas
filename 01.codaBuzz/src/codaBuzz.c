aa1a8aa246b6c8c62c41b7196af2c9ae55ffe7b9
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-30 10:40:38 +0200

    feat: handle negative numbers

--- 01.codaBuzz/src/codaBuzz.c
+++ 01.codaBuzz/src/codaBuzz.c
@@ -1,5 +1,10 @@
 #include "codaBuzz.h"
 
+#include <stddef.h>
+
 const char *coda_word(const int n) {
+    if (n < 1) {
+        return NULL;
+    }
     return "1";
 }
--- 01.codaBuzz/src/test_codaBuzz.c
+++ 01.codaBuzz/src/test_codaBuzz.c
@@ -3,11 +3,16 @@
 #include <stdio.h>
 #include "codaBuzz.h"
 
+void out_of_range_for_negative_numbers(void) {
+    assert(coda_word(-1) == NULL);
+}
+
 void returns_number_for_classic_ones(void) {
     assert(strcmp(coda_word(1), "1") == 0);
 }
 
 int main(void) {
+    out_of_range_for_negative_numbers();
     returns_number_for_classic_ones();
 
     printf("✅ All tests ran successfully\n");
