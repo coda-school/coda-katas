82a716c07c38cc65459d1d220a8338e85497fcf0
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-30 10:43:06 +0200

    feat: add edge cases

--- 01.codaBuzz/src/test_codaBuzz.c
+++ 01.codaBuzz/src/test_codaBuzz.c
@@ -1,10 +1,14 @@
 #include <assert.h>
+#include <limits.h>
 #include <string.h>
 #include <stdio.h>
+
 #include "codaBuzz.h"
 
 void out_of_range_for_negative_numbers(void) {
     assert(coda_word(-1) == NULL);
+    assert(coda_word(0) == NULL);
+    assert(coda_word(INT_MIN) == NULL);
 }
 
 void returns_number_for_classic_ones(void) {
