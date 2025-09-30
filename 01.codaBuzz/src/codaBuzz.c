13f4542900d7c7c58ef3040fa5a089bcf60f298e
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-30 13:50:52 +0200

    refactor: add constants for CODA

--- 01.codaBuzz/src/codaBuzz.c
+++ 01.codaBuzz/src/codaBuzz.c
@@ -3,8 +3,12 @@
 #include <stddef.h>
 #include <stdio.h>
 
-const int lowerBound = 1;
-const int greaterBound = 100;
+#define LOWER_BOUND 1
+#define UPPER_BOUND 100
+
+static const char *const CODA = "Coda";
+static const char *const BUZZ = "Buzz";
+static const char *const CODABUZZ = "CodaBuzz";
 
 const char *number_to_string(const int n) {
     static char buffer[12];
@@ -13,16 +17,18 @@ const char *number_to_string(const int n) {
 }
 
 int is_out_of_range(const int n) {
-    return n < lowerBound || n > greaterBound;
+    return n < LOWER_BOUND || n > UPPER_BOUND;
+}
+
+const char *convert_safely(const int n) {
+    if (n % 15 == 0) return CODABUZZ;
+    if (n % 3 == 0) return CODA;
+    if (n % 5 == 0) return BUZZ;
+
+    return number_to_string(n);
 }
 
 const char *convert(const int n) {
-    if (is_out_of_range(n)) {
-        return NULL;
-    }
-    if (n % 15 == 0) return "CodaBuzz";
-    if (n % 3 == 0) return "Coda";
-    if (n % 5 == 0) return "Buzz";
-
-    return number_to_string(n);
+    if (is_out_of_range(n)) return NULL;
+    return convert_safely(n);
 }
