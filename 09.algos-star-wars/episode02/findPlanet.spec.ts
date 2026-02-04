a98b778393186647cf3ce72e13b8249016fa5463
Yoan Thirion <yoan.thirion@gmail.com>
2026-02-04 15:07:35 +0100

    feat: add solution for episode 02

--- 09.algos-star-wars/episode02/findPlanet.spec.ts
+++ 09.algos-star-wars/episode02/findPlanet.spec.ts
@@ -1,19 +1,21 @@
-import { describe, it, expect } from "vitest"
-import {findPlanet, Planet} from "./findPlanet";
+import {describe, expect, it} from "vitest"
+import {findPlanet, findPlanetWithoutFor, Planet} from "./findPlanet";
 
 const galaxy: Planet = {
     name: "Coruscant",
     satellites: [
-        { name: "Tatooine", satellites: [{ name: "Naboo", satellites: [{ name: "Alderaan", satellites: [] }] }] },
-        { name: "Hoth", satellites: [{ name: "Dagobah", satellites: [] }, { name: "Endor", satellites: [] }] },
-        { name: "Mustafar", satellites: [{ name: "Geonosis", satellites: [] }] }
+        {name: "Tatooine", satellites: [{name: "Naboo", satellites: [{name: "Alderaan", satellites: []}]}]},
+        {name: "Hoth", satellites: [{name: "Dagobah", satellites: []}, {name: "Endor", satellites: []}]},
+        {name: "Mustafar", satellites: [{name: "Geonosis", satellites: []}]}
     ]
 }
 
 describe("Episode 2 : l'arbre des Planètes", () => {
     it("finds planets in the galaxy", () => {
-        const targets = ["Naboo","Endor","Geonosis","Coruscant","Hoth","Tatooine","Dagobah","Alderaan","Mustafar","Yavin"]
-        const expected = [true,true,true,true,true,true,true,true,true,false];
-        targets.forEach((t,i)=> expect(findPlanet(galaxy,t)).toBe(expected[i]));
+        const targets = ["Naboo", "Endor", "Geonosis", "Coruscant", "Hoth", "Tatooine", "Dagobah", "Alderaan", "Mustafar", "Yavin"]
+        const expected = [true, true, true, true, true, true, true, true, true, false];
+
+        targets.forEach((t, i) => expect(findPlanet(galaxy, t)).toBe(expected[i]));
+        targets.forEach((t, i) => expect(findPlanetWithoutFor(galaxy, t)).toBe(expected[i]));
     })
 })
--- 09.algos-star-wars/episode02/findPlanet.ts
+++ 09.algos-star-wars/episode02/findPlanet.ts
@@ -4,6 +4,14 @@ export type Planet = {
 }
 
 export function findPlanet(root: Planet, target: string): boolean {
-    // TODO
+    if (root.name === target) return true
+    for (const satellite of root.satellites) {
+        if (findPlanet(satellite, target)) return true
+    }
     return false
+}
+
+export function findPlanetWithoutFor(root: Planet, target: string): boolean {
+    if (root.name === target) return true
+    return root.satellites.find(satellite => findPlanet(satellite, target)) !== undefined;
 }
