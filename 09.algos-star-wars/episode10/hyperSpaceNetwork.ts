a789467ccc58727b2529930dbf1bbc87a1b77c8e
Yoan Thirion <yoan.thirion@gmail.com>
2026-02-04 21:40:04 +0100

    feat: add semantic for day 10

--- 09.algos-star-wars/episode10/hyperSpaceNetwork.ts
+++ 09.algos-star-wars/episode10/hyperSpaceNetwork.ts
@@ -1,25 +1,23 @@
+type Planet = string;
+
 export type HyperspaceNetwork = {
-    [planet: string]: string[]
+    [planet: Planet]: Planet[]
+}
+
+const dfs = (network: HyperspaceNetwork,
+             current: Planet,
+             destination: Planet,
+             visited: Set<Planet>) => {
+    if (current == destination) return true;
+    if (visited.has(current)) return false;
+
+    for (const neighbor of network[current] || []) {
+        if (dfs(network, neighbor, destination, visited)) return true;
+    }
+    return false;
 }
 
 export const canReach = (
     network: HyperspaceNetwork,
-    start: string,
-    destination: string): boolean => dfs(network, start, destination, new Set<string>());
-
-function dfs(network: HyperspaceNetwork,
-             planet: string,
-             destination: string,
-             visited: Set<string>): boolean {
-
-    if (planet === destination) return true;
-    if (visited.has(planet)) return false;
-
-    visited.add(planet);
-
-    for (const neighbor of network[planet] || []) {
-        if (dfs(network, neighbor, destination, visited)) return true;
-    }
-
-    return false;
-}
+    start: Planet,
+    destination: Planet): boolean => dfs(network, start, destination, new Set<Planet>())
