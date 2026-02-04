b9754ad6a97d04e68f136697fb6f342eeef1bacd
Yoan Thirion <yoan.thirion@gmail.com>
2026-02-04 15:03:05 +0100

    feat: add solution for episode 00

--- 09.algos-star-wars/episode00/midichlorians.spec.ts
+++ 09.algos-star-wars/episode00/midichlorians.spec.ts
@@ -1,15 +1,21 @@
 import {describe, expect, it} from "vitest"
-import {totalMidichlorians} from "./midichlorians";
+import {totalMidichlorians, totalMidichloriansWithForeach, totalMidichloriansWithReducer} from "./midichlorians";
+
+let jedis = [
+    {name: "Luke", midichlorians: 8000},
+    {name: "Yoda", midichlorians: 12000}
+];
 
 describe('Episode 0: les Midichloriens', () => {
     it("sums all midichlorians", () => {
-        expect(totalMidichlorians([
-            {name: "Luke", midichlorians: 8000},
-            {name: "Yoda", midichlorians: 12000}
-        ])).toBe(20000);
+        expect(totalMidichlorians(jedis)).toBe(20000);
+        expect(totalMidichloriansWithForeach(jedis)).toBe(20000);
+        expect(totalMidichloriansWithReducer(jedis)).toBe(20000);
     });
 
     it("handles empty list", () => {
         expect(totalMidichlorians([])).toBe(0);
+        expect(totalMidichloriansWithForeach([])).toBe(0);
+        expect(totalMidichloriansWithReducer([])).toBe(0);
     });
 });
--- 09.algos-star-wars/episode00/midichlorians.ts
+++ 09.algos-star-wars/episode00/midichlorians.ts
@@ -1,5 +1,21 @@
 export type Jedi = { name: string; midichlorians: number }
 
 export function totalMidichlorians(jedis: Jedi[]): number {
-    return 0;
+    let midichlorians = 0;
+    for (let i = 0; i < jedis.length; i++) {
+        midichlorians += jedis[i].midichlorians;
+    }
+    return midichlorians;
+}
+
+export function totalMidichloriansWithForeach(jedis: Jedi[]): number {
+    let midichlorians = 0;
+    for (const jedi of jedis) {
+        midichlorians += jedi.midichlorians;
+    }
+    return midichlorians;
+}
+
+export function totalMidichloriansWithReducer(jedis: Jedi[]): number {
+    return jedis.reduce((midichlorians, jedi) => jedi.midichlorians + midichlorians, 0);
 }
